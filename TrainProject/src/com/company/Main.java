4325ab7ca9ce9e2695246b559a5cb66db0452c19
maayanpolitzer <maayanpolitzer@Maayans-MacBook-Air.local>
2016-02-24 23:03:29 +0200

    24.2.2016 - MUST DO HOMEWORK!!!!!
    send me the resolved project or questions by mail

---
+++ TrainProject/src/com/company/Main.java
@@ -0,0 +1,49 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*
+             Send me the resoled project to: maayanp163@gmail.com
+             until our next meeting at sunday - 28.2.2016!!!!!
+             OR
+             send me the reason why the project is not ready.
+
+             **********  THIS IS A MUST ("חובה").  *************
+         */
+
+        /*
+        WHAT TO DO:
+            General info:
+                We will create a program for the "ISRAEL TRAIN", that our customers can
+                travel by train.
+
+                Every customer will have:
+                    1. name.
+                    2. id.
+                    3. ridesLeft (starts with zero).
+                    4. inStation (starts with false).
+
+                The customer can do:
+                    1. recharge card - to get more rides ( in order to enter a station ).
+                    2. enter a station (to travel) - if he is not already in a station,
+                       and if he have rides left (subtract a ride).
+                    3. leave a station (to go home) - if he is in a station.
+
+
+                Please check if it is working without bugs.
+
+              YOU CAN ADD MORE PRIVATE FIELDS OR METHODS IF YOU WANT.
+
+              If you have any question, you can ask me by mail.
+
+              GOOD LUCK,
+
+              MAAYAN.
+
+         */
+
+
+
+    }
+}
