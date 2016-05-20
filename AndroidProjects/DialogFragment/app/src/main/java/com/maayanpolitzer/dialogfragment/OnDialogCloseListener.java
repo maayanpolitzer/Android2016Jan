ac60cb95a1f18dfca71e1421da4953bf14c52765
maayanpolitzer <maayanpolitzer@Maayans-MacBook-Air.local>
2016-05-20 09:23:12 +0300

    18.5.16

--- AndroidProjects/DialogFragment/app/src/main/java/com/maayanpolitzer/dialogfragment/OnDialogCloseListener.java
+++
@@ -1,8 +0,0 @@
-package com.maayanpolitzer.dialogfragment;
-
-/**
- * Created by maayanpolitzer on 20/05/2016.
- */
-public interface OnDialogCloseListener {
-    void onClose(String username, String password);
-}
